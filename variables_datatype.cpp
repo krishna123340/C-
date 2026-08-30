f6889fcdff361e76bdaa63583d42c92c73c5fac1
Krishna Singh <krishnasingh@Krishnas-MacBook-Air.local>
2026-08-30 14:07:30 +0530

    Added C++ practice programs

---
+++ variables_datatype.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+ 
+int main () {
+
+    //variable define
+    //int age = 25;
+    // cout << " Babber's Age "
+
+    // decalartion 
+    //int age;
+    //cout << age;
+
+    //defination  
+    // int age = 21;
+   // cout << age <<endl;
+
+    //manipulation or updation
+    //age =101;
+    //cout << age <<endl;
+
+    // //int
+    // int count =5;
+    // //float
+    // float share = 3.14;
+    // //char
+    // char alaphabet ='z';
+    // //double
+    // double weight = 55.69887;
+    // //boolean
+    // bool ismale = true;
+    // bool ischild = 1;
+    // bool isfemale = false;
+    // bool isgirl = 0;
+
+    // cout<<count<<endl;
+    // cout<< share<<endl;
+    // cout<< alaphabet<<endl;
+    // cout<<weight<<endl;
+    // cout<<ismale<<endl;
+
+    int age = 12;
+    cout << sizeof(age)<<endl;
+    return 0;
+
+}

6786c74cfec681dbf3315bf721cae70b90daf551
Krishna Singh <krishnasingh@Krishnas-MacBook-Air.local>
2026-08-31 22:07:57 +0530

    Added user input practice

--- User.input.cpp
+++ User.input.cpp
@@ -3,17 +3,16 @@ using namespace std;
 
 int main () {
 
-  //
+  // variable decalare
+  int marks;
+  cout <<"Enter your marks" << endl;
 
+  // input le rhe h yha pr 
+  cin >> marks;
+  cout<< "your marks:"<< marks <<endl;
 
-
-
-
-
+return 0;
 
 
 }
-
-
-
 

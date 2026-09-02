310054e6a98b49b11b3e1f2523c040e295c68952
Krishna Singh <krishnasingh@Krishnas-MacBook-Air.local>
2026-09-02 13:58:06 +0530

    Added c++ prectice program

---
+++ operator.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // int a = 5;
+
+    // cout << a << endl;
+    // cout << (++a) << endl;
+    // // a ki value 6 ho chuki hai
+
+    // cout << (a++) << endl;
+    // // print kara to 6 hi print hoga, but a ki value 7 ho chuki hogi
+    // cout << a << endl;
+
+    // cout << (--a) << endl;
+    // // a ki value 6 ban chuki hai
+
+    // cout << (a--) << endl;
+    // // print toh 6 hi hoga but a ki value 5 ban chuki hai
+    // cout << a << endl;
+
+    //Arithmetic
+    // int a = 10;
+    // int b = 5;
+
+    // cout<< (a+b) <<endl;
+    // cout<< (a-b) <<endl;
+    // cout<< (a*b) <<endl;
+    // cout<< (a/b) <<endl;
+    // cout<< (a%b) <<endl;
+    
+    // cout<< (5/2) <<endl;    // int/int = int| esliye output 2 aaya
+    
+    // bool cond1 = (10>5);
+    // bool cond2 = (5>=5);
+    // bool cond3= (2!=5);
+
+    // if(cond1 && cond2 && cond3){
+    //     cout<<"all conditions are true"<<endl;
+
+    // }
+    // else {
+    //     cout <<"all condition are not true"<<endl;
+
+    // }
+    
+    // bool condition = false;
+    // cout << !condition <<endl;   // ! ye false to true or true to false kr deta h
+
+    //  int arr =5;
+    //  cout << arr <<endl;
+
+    // // arr = arr + 10;
+    // arr += 10;
+    // cout << arr << endl;
+  
+     cout<< (~1) <<endl;
+     cout<< (5 << 4) <<endl;
+     cout<< (100 >> 1) <<endl;
+     cout<< (5^5) <<endl;
+
+    return 0;
+}

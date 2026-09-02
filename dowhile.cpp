a4219ddae1373f9ec9f6e0a7e9b23e64d8ef45cd
Krishna Singh <krishnasingh@Krishnas-MacBook-Air.local>
2026-09-02 12:18:51 +0530

    Added dowhile practice

---
+++ dowhile.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    // for (int i=1;i <= 5; i = i+1) {
+    //    cout << i <<" ";
+    
+    // }
+    // cout << endl;
+
+    // int i= 1;
+    // do {
+    //  //logic
+    //  cout<<i <<" ";
+    //  //updation
+    //  i = i+1;
+    // }while(i<=5);
+
+    // cout <<endl;
+
+     for(int i = 1; i<=3; i= i+1) {
+         for(int j=1; j<=3;j=j+1) {
+              cout << i  <<j << " ";
+
+         }
+          cout << endl;
+         
+        }
+    return 0;
+
+    }
+

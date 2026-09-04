686c84ff1991b02177bb10f02ef29fb37f102d17
Krishna Singh <krishnasingh@Krishnas-MacBook-Air.local>
2026-09-04 19:55:16 +0530

    Added c++ prectice file

---
+++ array.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+    
+  void printArray(int arr[], int size) {
+      //printing
+      for(int index=0; index<=size-1; index++) {
+        cout <<arr[index] <<" ";
+
+      }
+
+  }
+
+
+ int main() {
+
+    // int arr[5] = {0};
+    // int size = 5;
+     
+    //   printArray(arr, size);
+
+    // Declaration
+    // int laxman[101];
+    // int arr[11];
+
+    // Initialization
+    // int brr[5] = {10, 20, 30, 40};
+    // int crr[] = {10, 20, 30};
+
+    // int multipleOf2[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    // // cout << multipleOf2[33] <<endl;
+    // Traverse
+    // for(int index = 0; index <= 9; index++) {
+    //     cout << multipleOf2[index] << " ";
+    // }
+
+    // Input in array
+    // int arr[5];
+
+    // for(int index = 0; index <= 4; index++) {
+    //     cout << "Enter the value for arr[" << index << "] : ";
+    //     cin >> arr[index];
+    //     cout << endl;
+    // }
+
+    
+    // for(int index = 0; index <= 4; index++) {
+    //     cout << arr[index] << " ";
+    // }
+
+    // int arr[] = {10,20,30,40,50};
+
+    // int sum = 0;
+    // for(int index=0; index<=4; index++){
+    //     sum = sum + arr[index];
+
+    // }
+    // cout << "Total sum:" << sum << endl;
+    
+
+    return 0;
+}
